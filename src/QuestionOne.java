a2ca646e1eb9cec910b4940d8d51afa4ee866faf
Code BY OSHAMA <text.oshama@gmail.com>
2024-03-03 09:24:29 -0600

    commit

--- src/QuestionOne.java
+++ src/QuestionOne.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class QuestionOne {
+public class QuestionOne {
+
+    /*
+
+    Print the given pattern for Solid Rectangle
+
+    *****
+    *****
+    *****
+    *****
+
+     */
+
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 4 ; i++) {
+            for(int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
 }
+
+
+/*
+        Time complexity of the code : O(n^2)
+        Space Complexity : O(1)
+ */
--- src/QuestionTwo.java
+++ src/QuestionTwo.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class QuestionTwo {
+public class QuestionTwo {
+
+    /*
+
+    Print the pattern for Hollow Rectangle
+
+    * * * * *
+    *       *
+    *       *
+    * * * * *
+
+
+     */
+
+    public static void main(String[] args) {
+
+        int rows = 4;
+        int columns = 5;
+
+        for(int i = 1; i <= rows; i++) {
+            for(int j = 1; j <= columns; j++) {
+                if(i == 1 || j == 1 || i == rows || j == columns){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
 }
